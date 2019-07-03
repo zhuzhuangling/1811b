98a85924d34b5c4147d9bea9bb05ee299e3ec201
e <2418653634@qq.com>
2019-07-03 16:31:00 +0800

    第一次提交

---
+++ src/main/java/com/jk/demogit1811b/Demogit1811bApplication.java
@@ -0,0 +1,13 @@
+package com.jk.demogit1811b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demogit1811bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demogit1811bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jk/demogit1811b/Demogit1811bApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jk.demogit1811b;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demogit1811bApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
