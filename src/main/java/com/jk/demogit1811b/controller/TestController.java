1b15805ff5e6beb2e86f4fcb7ad67023a2a7a754
rootslm <410949413@qq.com>
2019-07-03 17:14:19 +0800

    first

---
+++ src/main/java/com/jk/demogit1811b/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.jk.demogit1811b.controller;
+
+/**
+ * @Author:slm
+ * @Date：
+ **/
+
+public class TestController {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
