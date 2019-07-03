a2a1741b9c309d5295c165c8c207b773ad2aa00c
gtt19960308 <980234538@qq.com>
2019-07-03 17:48:34 +0800

    gtt的修改

---
+++ src/main/java/com/jk/demogit1811b/asn/uuu.java
@@ -0,0 +1,12 @@
+package com.jk.demogit1811b.asn;
+
+/**
+ * @ClassName uuu
+ * @Description TODO
+ * @Author gtt
+ * @Date 2019/7/3 17:47
+ * @Version 1.0
+ **/
+public class uuu {
+    private Integer id;
+}
